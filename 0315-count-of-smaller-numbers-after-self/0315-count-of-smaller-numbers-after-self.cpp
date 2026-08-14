ea269477bb0b57f80d8587e0c7ff90d0287f9f50
kushagra1007 <jainkushagra131@gmail.com>
2026-08-14 22:09:13 +0530

    Time: 101 ms (79.49%), Space: 99.1 MB (75.83%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,63 +1,70 @@
 class Solution {
 public:
     vector<int> counts;
+    vector<pair<int, int>> temp;
 
-    void mergeSortCount(vector<pair<int,int>>& arr, int left, int right) {
+    void mergeSort(vector<pair<int, int>>& arr, int left, int right) {
         if (left >= right) return;
 
         int mid = left + (right - left) / 2;
-        mergeSortCount(arr, left, mid);
-        mergeSortCount(arr, mid + 1, right);
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
         merge(arr, left, mid, right);
     }
 
-    void merge(vector<pair<int,int>>& arr, int left, int mid, int right) {
-        vector<pair<int,int>> merged;
-        int i = left, j = mid + 1;
+    void merge(vector<pair<int, int>>& arr, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
         int rightCount = 0;
 
         while (i <= mid && j <= right) {
-            if (arr[j].first < arr[i].first) {
-                rightCount++;
-                merged.push_back(arr[j]);
-                j++;
-            } else {
-                // arr[i].first <= arr[j].first, take from left
-                counts[arr[i].second] += rightCount;
-                merged.push_back(arr[i]);
-                i++;
-            }
-        } 
 
-        // leftover left elements: all of rightCount applies (right half exhausted)
+            if (arr[j].first < arr[i].first) {
+                // This right element is smaller than arr[i]
+                rightCount++;
+                temp[k++] = arr[j++];
+            }
+            else {
+                // All previously selected right elements
+                // are smaller than arr[i]
+                counts[arr[i].second] += rightCount;
+                temp[k++] = arr[i++];
+            }
+        }
+
+        // Remaining left elements
         while (i <= mid) {
             counts[arr[i].second] += rightCount;
-            merged.push_back(arr[i]);
-            i++;
+            temp[k++] = arr[i++];
         }
 
-        // leftover right elements: just copy, no counting needed
+        // Remaining right elements
         while (j <= right) {
-            merged.push_back(arr[j]);
-            j++;
+            temp[k++] = arr[j++];
         }
 
-        // copy merged back into original array range
-        for (int k = left; k <= right; k++) {
-            arr[k] = merged[k - left];
+        // Copy sorted range back
+        for (int p = left; p <= right; p++) {
+            arr[p] = temp[p];
         }
     }
 
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        counts.assign(n, 0);
 
-        vector<pair<int,int>> arr(n);
+        counts.assign(n, 0);
+        temp.resize(n);
+
+        vector<pair<int, int>> arr(n);
+
         for (int i = 0; i < n; i++) {
             arr[i] = {nums[i], i};
         }
 
-        mergeSortCount(arr, 0, n - 1);
+        mergeSort(arr, 0, n - 1);
 
         return counts;
     }
