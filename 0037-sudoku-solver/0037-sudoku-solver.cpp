503a2b3dd6c5daecd2923a10a4e64254f1cefd0a
kushagra1007 <jainkushagra131@gmail.com>
2026-08-20 16:40:41 +0530

    Time: 94 ms (93.32%), Space: 9.1 MB (5.57%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,56 +1,78 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>& board,int row,int col,int dig){
-        // horizontal
-        for(int i=0;i<9;i++){
-            if(board[row][i] == dig){
-                return false;
-            }
-        }
-        // horizontal
-        for(int i=0;i<9;i++){
-            if(board[i][col] == dig){
-                return false;
-            }
-        }
-        // grid
-        int srow = (row/3)*3;
-        int scol = (col/3)*3;
-        for(int i=srow;i<=srow+2;i++){
-            for(int j=scol;j<=scol+2;j++){
-                if(board[i][j] == dig){
-                    return false;
-                }
-            }
-        }
-        return true;
-    } 
-    bool helper(vector<vector<char>>& board,int row, int col){
-        if(row == 9){
+
+    bool helper(vector<vector<char>>& board,
+                int pos,
+                vector<vector<bool>>& row,
+                vector<vector<bool>>& col,
+                vector<vector<bool>>& box) {
+
+        // All cells filled
+        if (pos == 81) {
             return true;
         }
-        int nextRow = row,nextCol = col+1;
-        if(nextCol == 9){
-            nextRow = row+1;
-            nextCol = 0;
+
+        int r = pos / 9;
+        int c = pos % 9;
+
+        // Already filled
+        if (board[r][c] != '.') {
+            return helper(board, pos + 1, row, col, box);
         }
-        if(board[row][col] != '.'){
-            return helper(board,nextRow,nextCol);
-        }
-        //place the digit
-        for(char dig='1';dig<='9';dig++){
-            if(isSafe(board,row,col,dig)){
-                board[row][col] = dig;
-                if(helper(board,nextRow,nextCol)){
-                    return true;
-                }   
-                board[row][col] = '.';
+
+        // Find box number
+        int b = (r / 3) * 3 + (c / 3);
+
+        for (int d = 1; d <= 9; d++) {
+
+            if (row[r][d] || col[c][d] || box[b][d]) {
+                continue;
             }
+
+            // Place digit
+            board[r][c] = char('0' + d);
+
+            row[r][d] = true;
+            col[c][d] = true;
+            box[b][d] = true;
+
+            if (helper(board, pos + 1, row, col, box)) {
+                return true;
+            }
+
+            // Backtrack
+            board[r][c] = '.';
+
+            row[r][d] = false;
+            col[c][d] = false;
+            box[b][d] = false;
         }
-        return false;    
+
+        return false;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-       helper(board,0,0);
+
+        vector<vector<bool>> row(9, vector<bool>(10, false));
+        vector<vector<bool>> col(9, vector<bool>(10, false));
+        vector<vector<bool>> box(9, vector<bool>(10, false));
+
+        // Initialize existing digits
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+
+                if (board[r][c] != '.') {
+
+                    int d = board[r][c] - '0';
+                    int b = (r / 3) * 3 + (c / 3);
+
+                    row[r][d] = true;
+                    col[c][d] = true;
+                    box[b][d] = true;
+                }
+            }
+        }
+
+        helper(board, 0, row, col, box);
     }
 };
